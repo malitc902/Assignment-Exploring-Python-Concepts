6cdee2050df4d05195b69a324adc7ba4302a2b5a
Chris M <110314359+malitc902@users.noreply.github.com>
2025-05-04 20:20:16 -0400

    Assignment: Exploring Python Concepts
    Task 1 - Variables: Your First Python Intro
    Task 2 - Operators: Playing with Numbers
    Task 3 - Conditional Statements: The Number Checker

---
+++ python.py
@@ -0,0 +1,21 @@
+#Task 1 - Variables: Your First Python Intro
+name = "Chris"
+age = 36
+height = 5.5
+print("Hi! My name is",name,"I am", age, "years old and my height is", height, "ft. It's nice to meet you!")
+
+#Task 2 - Operators: Playing with Numbers
+num1 = 7
+num2 = 29
+print("I came to Canada at the age of", num1, ".", "I've been living here for", num2, "is", num1 + num2, "Dang! I feel old!")
+#I tried to create a story using variables and print statements, and adding a bit of humour in said statement. I hope you like it!
+
+#Task 3 - Conditional Statements: The Number Checker
+age = int(input("Please enter a number: "))
+if age > 0:
+    print("This is a positive number. Awesome!")
+elif age < 0:
+    print("This is a negative number. Better luck next time!")
+else:
+    print("Zero it is. WowA perfect balance!")
+#I name variable and used the input function to get a number from the user and then used if, elif, and else statements to check if the number is positive, negative, or zero. I hope you like it!
